3541b6dd4cea687e659a84e73ba55c5bd381b3c8
doge-gif <57514679+doge-gif@users.noreply.github.com>
2023-02-28 01:28:48 +0900

    bugfix: don;t break the current API

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    scripts=["scripts/topic_rate_watchdog"],
+    scripts=["scripts/topic_rate_watchdog.py"],
     packages=[pathlib.Path(__file__).parent.name],
     package_dir={"": "src"},
 )
